b6c31dfd739441bc4ca57f8e62ffa45498e26ab5
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-15 12:58:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,6 @@
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |array| 
+  data[season].each do |array|
+    if array["status"] == "Winner" 
 end
 
 def get_contestant_name(data, occupation)
